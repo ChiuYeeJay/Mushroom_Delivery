66ffcf52e48d6d384334d35e188ba61b953e9650
ChiuYeeJay <56674515+ChiuYeeJay@users.noreply.github.com>
2021-01-31 03:12:26 -0800

    Add files via upload
    better progress bar

--- main.cpp
+++ main.cpp
@@ -24,13 +24,11 @@ using namespace CryptoPP;
 using boost::asio::ip::tcp;
 
 //TODO deal with the problem of nthl(), htnl()
-//TODO show byte rate
 //TODO add "agree" progress
 //TODO add available file list
 //TODO add history ip list
 //TODO add setting file (bar's len)
 //TODO delete rcv file when error happened
-//TODO show time consumption
 
 asio::ip::address typein_ip_address(){
     string in;
@@ -80,26 +78,39 @@ string byte_format(long long b){
 }
 
 void show_progress_bar(const long long file_size, const long long cur_size){
-    static chrono::steady_clock::time_point last_clk;
-    static int last_size;
+    static chrono::steady_clock::time_point last_clk, last_update_byte_rate_time;
+    static int last_size ,br_last_size;
+    static double last_byte_rate;
     static int last_str_len;
     string bar, byte_str, size_str, out;
     chrono::steady_clock::time_point now_clk = chrono::steady_clock::now();
     double progress_rate;
     double byte_rate;
-    double time_diff;
-    if(last_size == 0){
+    double time_diff, update_byte_rate_time_diff;
+    if(cur_size == 0){
         last_size = cur_size;
         last_clk = now_clk;
+        last_update_byte_rate_time = now_clk;
         progress_rate = cur_size*1.0/file_size;
         byte_rate = 0;
+        last_byte_rate = 0;
+        br_last_size = 0;
     }
     else{
         time_diff = chrono::duration_cast<chrono::milliseconds>((now_clk-last_clk)).count()/1000.0;
+        update_byte_rate_time_diff = chrono::duration_cast<chrono::milliseconds>((now_clk-last_update_byte_rate_time)).count()/1000.0;
         if(time_diff < 0.01 && file_size>cur_size) return;
         else{
             progress_rate = cur_size*1.0/file_size;
-            byte_rate = 1.0*(cur_size-last_size)/time_diff;
+            if(update_byte_rate_time_diff > 0.3){
+                byte_rate = 1.0*(cur_size-br_last_size)/update_byte_rate_time_diff;
+                last_byte_rate = byte_rate;
+                br_last_size = cur_size;
+                last_update_byte_rate_time = now_clk;
+            }
+            else{
+                byte_rate = last_byte_rate;
+            }
             last_clk = now_clk;
             last_size = cur_size;
             while(last_str_len--) printf("\b");
@@ -117,7 +128,13 @@ void show_progress_bar(const long long file_size, const long long cur_size){
     size_str = " " + size_str.substr(0, min(size_str.find('.')+2, size_str.size())) + "%";
     out = bar + size_str + byte_str;
     last_str_len = out.size();
-    printf("%s", out.data());
+    cout << out;
+    cout.flush();
+}
+
+string time_format(long long t){
+    string u = to_string((t/1000000)/1000.0);
+    return u.substr(0, min(u.find('.')+4, u.size())) + " s";
 }
 
 void exception_exit(const char* from, tcp::socket& socket){
@@ -128,6 +145,7 @@ void exception_exit(const char* from, tcp::socket& socket){
 
 void exception_exit(string from, tcp::socket& socket){
     socket.close();
+    cout << endl;
     cout << "Error: " << from << endl;
     exit(1);
 }
@@ -157,6 +175,9 @@ int main(){
     unsigned long long file_size;
     string file_name;
     string crypt_name;
+
+    //*about time
+    chrono::steady_clock::time_point time_stamp;
     
 
     //> Waiting to accept or typing IP and connecting to other
@@ -173,15 +194,16 @@ int main(){
     // cout << "$set socket" << endl;
     try{
         if(cw_ans == "c"){
-            cout << "connecting..." << endl;
             end_point = tcp::endpoint(typein_ip_address(), 50000);
             // end_point = tcp::endpoint(asio::ip::address::from_string("127.0.0.1"), 50000);
+            cout << "connecting...";
             socket.connect(end_point);
             cout << "success! You are client!" << endl;
             is_server = false;
         }
         else{
-            cout << "waiting..." << endl;
+            cout << "waiting...";
+            cout.flush();
             acceptor = tcp::acceptor(io_service, tcp::endpoint(tcp::v4(), 50000));
             acceptor.accept(socket);
             cout << "connected! You are server!" << endl;
@@ -194,7 +216,9 @@ int main(){
     }
 
     //> Exchange AES key
-    cout << "preparing cryption..." << endl;
+    cout << "preparing cryption...";
+    cout.flush();
+    time_stamp = chrono::steady_clock::now();
     try{
         if(is_server){
             //* generate RSA
@@ -303,7 +327,7 @@ int main(){
 
         //* after exchanging
         // cout << "$after exchanging" << endl;
-        cout << "complete!" << endl;
+        cout << "complete! (" << time_format((chrono::steady_clock::now() - time_stamp).count()) << ")" << endl;
     }
     catch(const std::exception& e){
         cout << "failed!" << endl;
@@ -348,7 +372,7 @@ int main(){
         }
         else{
             boost::array<char, 128> rcv_buf = boost::array<char, 128>();
-            cout << "Waiting server's choice..." << endl;
+            cout << "Waiting server's choice...";
             socket.wait(socket.wait_read);
             socket.read_some(asio::buffer(rcv_buf));
             socket.wait(socket.wait_write);
@@ -378,7 +402,7 @@ int main(){
         ifstream source_file;
         cout << "You are sender! Choose file to deliver!" << endl;
         do{
-            cout << "Your file(name/path): ";
+            cout << "Your file(name): ";
             cin >> file_name;
             source_file.open(file_name, ios::binary | ios::in);
             if(source_file.fail()) cout << "Invalid file name! Input again!" << endl;
@@ -387,6 +411,9 @@ int main(){
         
         //* set AES encryption
         // cout << "$set AES encryption" << endl;
+        cout << "Encrypting your file...";
+        cout.flush();
+        time_stamp = chrono::steady_clock::now();
         CFB_Mode<AES>::Encryption cfbEncryption(aes_key, aes_key.size(), aes_iv);
 
         //* encrypting the file
@@ -403,9 +430,12 @@ int main(){
             cerr << e.what() << endl;
             exception_exit("something wrong when sender encrypt the file", socket);
         }
+        cout << "complete! (" << time_format((chrono::steady_clock::now()-time_stamp).count()) << ")" << endl;
     }
     else{
-        cout << "You are receiver! Waiting sender selecting and processing file..." << endl;
+        cout << "complete! You are receiver!" << endl;
+        cout << "Waiting sender selecting and processing file...";
+        cout.flush();
     }
 
     //> Preparing Delivery
@@ -428,7 +458,7 @@ int main(){
             socket.write_some(asio::buffer(file_info));
             socket.wait(socket.wait_read);
             socket.read_some(asio::buffer(rcv_buf));
-            if(rcv_buf.data() != ACK) exception_exit("sending file name to rcver, replying other than ACK", socket);
+            if(rcv_buf.data() != ACK) exception_exit("sending file info to rcver, replying other than ACK", socket);
         }
         catch(const std::exception &e){
             cerr << e.what() << endl;
@@ -445,6 +475,7 @@ int main(){
             socket.read_some(asio::buffer(rcv_buf));
             socket.wait(socket.wait_write);
             if(rcv_buf.data() != CMD_SND_INFO) exception_exit("receive other than cmd SND_NAME", socket);
+            cout << "complete!"<< endl;
             rcv_buf = boost::array<char, 1024>();
             socket.write_some(asio::buffer(ACK));
             socket.wait(socket.wait_read);
@@ -464,6 +495,7 @@ int main(){
     }
 
     //> Delivery
+    time_stamp = chrono::steady_clock::now();
     if(is_sender){
         //* send file
         // cout << "$send file" << endl;
@@ -474,6 +506,7 @@ int main(){
         try{
             cipher_file.open(crypt_name, ios::binary | ios::in);
             if(cipher_file.fail()) exception_exit("something wrong when reopen encrypted file", socket);
+            show_progress_bar(file_size, 0);
             do{
                 memset(str, 0, (MAX_FRAG)*sizeof(char));
                 if(remain_size < MAX_FRAG){
@@ -507,6 +540,7 @@ int main(){
             socket.read_some(asio::buffer(rcv_buf));
             if(rcv_buf.data() != ACK) exception_exit(string("sending complete, receiving other than ACK") + rcv_buf.data(), socket);
             socket.close();
+            cout << "file delivered successully! (" << time_format((chrono::steady_clock::now() - time_stamp).count()) << ")" << endl;
         }
         catch(const std::exception &e){
             cerr << e.what() << endl;
@@ -524,6 +558,7 @@ int main(){
             ofstream recover_file;
             cipher_file.open(crypt_name, ios::binary | ios::out);
             err.clear();
+            show_progress_bar(file_size, cur_size);
             while(1){
                 memset(str, 0, (MAX_FRAG)*sizeof(char));
                 socket.wait(socket.wait_read);
@@ -546,11 +581,13 @@ int main(){
             if(socket.is_open()) socket.close();
             exit(1);
         }
-        cout << "file delivered successully!" << endl;
+        cout << "file delivered successully! (" << time_format((chrono::steady_clock::now() - time_stamp).count()) << ")" << endl;
 
         //* decrypt file
         // cout << "$decrypt file" << endl;
-        cout << "decrypting..." << endl;
+        cout << "decrypting...";
+        cout.flush();
+        time_stamp =chrono::steady_clock::now();
         try{
             ifstream cipher_file;
             ofstream recover_file;
@@ -567,6 +604,7 @@ int main(){
             cout << "something wrong when decrypting file and close files" << endl;
             exit(1);
         }
+        cout << "complete! (" << time_format((chrono::steady_clock::now()-time_stamp).count()) << ")" << endl;
     }
 
     //> after delivery
@@ -576,5 +614,5 @@ int main(){
     catch(const std::exception &e){
         cout << "something wrong when deleting encrypted file, but who cares?";
     }
-    cout << "File delivery complete, thank you for using!" << endl;
+    cout << "Mission complete, thank you for using!" << endl;
 }
